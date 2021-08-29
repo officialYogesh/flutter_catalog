f89b5c109302237ff8fd934841c92ed4b2111dc8
Yogesh Patil <officialyogeshp@gmail.com>
2021-08-29 22:52:46 +0530

    Modified home layout, added Velocity_x package

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 
 import 'dart:convert';
+
+import 'package:velocity_x/velocity_x.dart';
 
 import 'package:flutter/services.dart';
 
@@ -27,27 +30,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    // * comment stash 01
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("Catalog App"),
+    //   ),
+    //   body: Padding(
+    //       padding: EdgeInsets.all(16.0),
+    //       child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+    //           ? ListView.builder(
+    //               itemCount: CatalogModel.items.length,
+    //               itemBuilder: (context, index) => ItemWidget(
+    //                     item: CatalogModel.items[index],
+    //                   ))
+    //           : Center(
+    //               child: CircularProgressIndicator(),
+    //             )),
+    //   drawer: MainDrawer(),
+    // );
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
-                  itemCount: CatalogModel.items.length,
-                  itemBuilder: (context, index) => ItemWidget(
-                        item: CatalogModel.items[index],
-                      ))
-              : Center(
+      backgroundColor: MyThemes.cream,
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CatalogHeader(),
+              if (CatalogModel.items.isNotEmpty)
+                CatalogList().expand()
+              else
+                Center(
                   child: CircularProgressIndicator(),
-                )),
-      drawer: MainDrawer(),
+                )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   void loadHomeData() async {
-    await Future.delayed(Duration(seconds: 5));
+    // await Future.delayed(Duration(seconds: 5));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
@@ -56,5 +80,98 @@ class _HomeScreenState extends State<HomeScreen> {
         .map<Item>((item) => Item.fromMap(item))
         .toList();
     setState(() {});
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyThemes.darkBluish).make(),
+        "Trending Products".text.make(),
+      ],
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItem({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: catalog.image),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              catalog.name.text.bold.lg.color(MyThemes.darkBluish).make(),
+              catalog.desc.text.gray500
+                  .subtitle2(context)
+                  // .textStyle(context.captionStyle)
+                  .make(),
+              10.heightBox,
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: EdgeInsets.zero,
+                children: [
+                  "\$${catalog.price}".text.bold.xl.make(),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(MyThemes.darkBluish),
+                          shape: MaterialStateProperty.all(StadiumBorder())),
+                      child: "Buy".text.make())
+                ],
+              ).pOnly(right: 8.0)
+            ],
+          ),
+        )
+      ],
+    )).white.rounded.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .p8
+        .color(MyThemes.cream)
+        .make()
+        .p16()
+        .w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -5,7 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 class MyThemes {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      // fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -16,4 +17,8 @@ class MyThemes {
 
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
+
+  // * Custom colors
+  static Color cream = Color(0xfff5f5f5);
+  static Color darkBluish = Color(0xff403b58);
 }
